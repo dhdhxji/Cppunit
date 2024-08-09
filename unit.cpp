f467bc79a066e24683968e4f4396d4d247ba185d
dhdhxji <keysindie@gmail.com>
2024-08-10 00:42:04 +0300

    Check unit compatibility before cast

--- unit.cpp
+++ unit.cpp
@@ -44,7 +44,6 @@ struct GetBaseUnit {
 
 
 // Remove Policy from name
-// std::ratio??
 template <typename TRatio>
 struct RatioConvPolicy
 {
@@ -61,11 +60,41 @@ struct RatioConvPolicy
 template <typename T, typename TUnit>
 using BaseUnit = Unit<T, TUnit, TUnit, ItselfConvPolicy>;
 
-// TODO is_compatible<Uni1, Unit2>::value
+
+template <typename TUnit>
+struct GetRootBaseUnit
+{
+private:
+    template <typename TUnitInternal, typename TBaseUnitInternal>
+    struct GetRootBaseUnitImpl
+    {
+        using type = typename GetRootBaseUnitImpl<TBaseUnitInternal, typename GetBaseUnit<TBaseUnitInternal>::type>::type;
+    };
+
+    template <typename TUnitInternal>
+    struct GetRootBaseUnitImpl<TUnitInternal, TUnitInternal>
+    {
+        using type = TUnitInternal;
+    };
+
+public:
+    using type = typename GetRootBaseUnitImpl<TUnit, typename GetBaseUnit<TUnit>::type>::type;
+};
+
+
+template <typename TUnit1, typename TUnit2>
+struct IsUnitCompatible : std::bool_constant<
+    std::is_same<
+        typename GetRootBaseUnit<TUnit1>::type,
+        typename GetRootBaseUnit<TUnit2>::type
+    >::value
+> {};
+
 
 template <typename TFrom, typename TTo>
 TTo unit_cast(const TFrom &unit)
 {
+    static_assert(IsUnitCompatible<TFrom, TTo>::value, "Units TFrom and TTo should have common base unit");
     const typename GetBaseUnit<TFrom>::type baseUnit = TFrom::ConvPolicy::toBase(unit);
     const TTo result = TTo::ConvPolicy::fromBase(baseUnit);
 
